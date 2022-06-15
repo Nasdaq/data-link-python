bfaa3b05ede7ae38501ff28b5da1b21500d12bef
Ren Ren <97460234+runawaycoast@users.noreply.github.com>
2022-06-15 09:45:44 -0400

    fix session object not getting through

--- test/test_authorized_session.py
+++ test/test_authorized_session.py
@@ -1,6 +1,4 @@
 from unittest import TestCase
-from unittest.mock import patch
-
 from nasdaqdatalink.model.authorized_session import AuthorizedSession
 from nasdaqdatalink.api_config import ApiConfig
 from requests.sessions import Session
@@ -18,7 +16,10 @@ class AuthorizedSessionTest(TestCase):
         self.assertTrue(isinstance(authed_session._api_config, ApiConfig))
 
     def test_authorized_session_pass_created_session(self):
+        ApiConfig.use_retries = True
+        ApiConfig.number_of_retries = 130
         authed_session = AuthorizedSession()
         self.assertTrue(isinstance(authed_session._auth_session, Session))
         adapter = authed_session._auth_session.get_adapter(ApiConfig.api_protocol)
         self.assertTrue(isinstance(adapter, HTTPAdapter))
+        self.assertEqual(adapter.max_retries.connect, 130)
